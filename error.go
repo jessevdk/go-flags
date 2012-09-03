e810b05c05e457270051f067385198fce562ff54
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2012-09-03 16:13:42 +0200

    Added documentation for the error type

--- error.go
+++ error.go
@@ -1,8 +1,10 @@
 package flags
 
+// ErrorType represents the type of error.
 type ErrorType uint
 
 const (
+	// Unknown or generic error
 	ErrUnknown ErrorType = iota
 
 	// Expected an argument but got none
@@ -21,11 +23,17 @@ const (
 	ErrNoArgumentForBool
 )
 
+// Error represents a parser error. The error returned from Parse is of this
+// type. The error contains both a Type and Message.
 type Error struct {
+	// The type of error
 	Type ErrorType
+
+	// The error message
 	Message string
 }
 
+// Get the errors error message.
 func (e *Error) Error() string {
 	return e.Message
 }
@@ -37,4 +45,7 @@ func newError(tp ErrorType, message string) error {
 	}
 }
 
+// The builtin help message was printed. This value will be returned from
+// Parse when the builtin help message was shown (i.e. when the ShowHelp
+// option is set and either -h or --help was specified on the command line).
 var ErrHelp = newError(ErrHelpShown, "help shown")
