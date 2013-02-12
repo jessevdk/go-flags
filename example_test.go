7e59e6d7807a1f4d21bdcb3c7dfba8e46c4a49de
Jesse van den Kieboom <jessevdk@gmail.com>
2013-02-12 10:01:52 +0100

    Added tests for pointers, slices and maps

--- example_test.go
+++ example_test.go
@@ -26,6 +26,18 @@ func Example() {
 
 		// Example of a value name
 		File string `short:"f" long:"file" description:"A file" value-name:"FILE"`
+
+		// Example of a pointer
+		Ptr *int `short:"p" description:"A pointer to an integer"`
+
+		// Example of a slice of strings
+		StringSlice []string `short:"s" description:"A slice of strings"`
+
+		// Example of a slice of pointers
+		PtrSlice []*string `long:"ptrslice" description:"A slice of pointers to string"`
+
+		// Example of a map
+		IntMap map[string]int `long:"intmap" description:"A map from string to int"`
 	}
 
 	// Callback which will invoke callto:<argument> to call a number.
@@ -42,6 +54,13 @@ func Example() {
 		"-vv",
 		"--offset=5",
 		"-n", "Me",
+		"-p", "3",
+		"-s", "hello",
+		"-s", "world",
+		"--ptrslice", "hello",
+		"--ptrslice", "world",
+		"--intmap", "a:1",
+		"--intmap", "b:5",
 		"arg1",
 		"arg2",
 		"arg3",
@@ -53,16 +72,25 @@ func Example() {
 	args, err := flags.ParseArgs(&opts, args)
 
 	if err != nil {
+		panic(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Verbosity: %d\n", len(opts.Verbose))
+	fmt.Printf("Verbosity: %v\n", opts.Verbose)
 	fmt.Printf("Offset: %d\n", opts.Offset)
 	fmt.Printf("Name: %s\n", opts.Name)
+	fmt.Printf("Ptr: %d\n", *opts.Ptr)
+	fmt.Printf("StringSlice: %v\n", opts.StringSlice)
+	fmt.Printf("PtrSlice: [%v %v]\n", *opts.PtrSlice[0], *opts.PtrSlice[1])
+	fmt.Printf("IntMap: [a:%v b:%v]\n", opts.IntMap["a"], opts.IntMap["b"])
 	fmt.Printf("Remaining args: %s\n", strings.Join(args, " "))
 
-	// Output: Verbosity: 2
+	// Output: Verbosity: [true true]
 	// Offset: 5
 	// Name: Me
+	// Ptr: 3
+	// StringSlice: [hello world]
+	// PtrSlice: [hello world]
+	// IntMap: [a:1 b:5]
 	// Remaining args: arg1 arg2 arg3
 }
