77a0da43c7e886c2733fc641602e1e91e48ea497
Jesse van den Kieboom <jessevdk@gmail.com>
2013-11-06 12:04:58 +0100

    Added tests for unknown flags

---
+++ unknown_test.go
@@ -0,0 +1,67 @@
+package flags
+
+import (
+	"testing"
+	"github.com/jessevdk/go-flags"
+)
+
+func TestUnknownFlags(t *testing.T) {
+	var opts = struct {
+		Verbose[] bool `short:"v" long:"verbose" description:"Verbose output"`
+	} {}
+
+	args := []string{
+		"-f",
+	}
+
+	p := flags.NewParser(&opts, 0)
+	args, err := p.ParseArgs(args)
+
+	if err == nil {
+		t.Fatal("Expected error for unknown argument")
+	}
+}
+
+func TestIgnoreUnknownFlags(t *testing.T) {
+	var opts = struct {
+		Verbose[] bool `short:"v" long:"verbose" description:"Verbose output"`
+	} {}
+
+	args := []string{
+		"hello",
+		"world",
+		"-v",
+		"--foo=bar",
+		"--verbose",
+		"-f",
+	}
+
+	p := flags.NewParser(&opts, flags.IgnoreUnknown)
+	args, err := p.ParseArgs(args)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	exargs := []string{
+		"hello",
+		"world",
+		"--foo=bar",
+		"-f",
+	}
+
+	issame := (len(args) == len(exargs))
+
+	if issame {
+		for i := 0; i < len(args); i++ {
+			if args[i] != exargs[i] {
+				issame = false
+				break
+			}
+		}
+	}
+
+	if !issame {
+		t.Fatalf("Expected %v but got %v", exargs, args)
+	}
+}
