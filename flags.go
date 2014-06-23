ec11066063266bcedd0dc996701cb4a8d482c2f3
Jesse van den Kieboom <jessevdk@gmail.com>
2014-06-23 13:58:39 +0200

    Only split completions on newlines

--- flags.go
+++ flags.go
@@ -208,6 +208,9 @@ calls the binary which supports go-flags completion:
         # All arguments except the first one
         args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
 
+        # Only split on newlines
+        local IFS=$'\n'
+
         # Call completion (note that the first element of COMP_WORDS is
         # the executable itself)
         COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} __complete -- "${args[@]}"))
