e874c5b242007bff7de59f24b02a4ad4379e3f9d
Pierre Curto <pierre.curto@gmail.com>
2016-02-26 18:45:18 +0100

    - added Option.Field() method to recover the struct field associated with the option - added Option.IsSetDefault() to help distinguish cases where the option was really set or set with default values

--- option.go
+++ option.go
@@ -86,6 +86,10 @@ type Option struct {
 	defaultLiteral string
 }
 
+func (option *Option) Field() reflect.Value {
+	return option.value
+}
+
 // LongNameWithNamespace returns the option's long name with the group namespaces
 // prepended by walking up the option's group tree. Namespaces and the long name
 // itself are separated by the parser's namespace delimiter. If the long name is
@@ -170,6 +174,11 @@ func (option *Option) Value() interface{} {
 // IsSet returns true if option has been set
 func (option *Option) IsSet() bool {
 	return option.isSet
+}
+
+// IsSetDefault returns true if option has been set with its default value.
+func (option *Option) IsSetDefault() bool {
+	return option.isSetDefault
 }
 
 // Set the value of an option to the specified value. An error will be returned
@@ -267,6 +276,7 @@ func (option *Option) clearDefault() {
 
 		for _, d := range usedDefault {
 			option.set(&d)
+			option.isSetDefault = true
 		}
 	} else {
 		tp := option.value.Type()
