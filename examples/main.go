206428b03a5152306e043b5c0b7c7575c24afc61
Denis Titusov <sescusu@gmail.com>
2019-10-06 14:19:11 +0300

    Implement error interface

--- examples/main.go
+++ examples/main.go
@@ -70,9 +70,13 @@ var parser = flags.NewParser(&options, flags.Default)
 
 func main() {
 	if _, err := parser.Parse(); err != nil {
-		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
-			os.Exit(0)
-		} else {
+		switch flagsErr := err.(type) {
+		case flags.ErrorType:
+			if flagsErr == flags.ErrHelp {
+				os.Exit(0)
+			}
+			os.Exit(1)
+		default:
 			os.Exit(1)
 		}
 	}
